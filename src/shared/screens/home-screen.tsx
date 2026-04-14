97fd2ae2f97bdf9ed7fed12a23197a244d29f399
saacostam <73088662+saacostam@users.noreply.github.com>
2026-04-14 01:44:37 -0500

    feat(home-screen): mock dashboard ui
    Mock dashboard elements:
    1. Header
    2. Statistics section

--- src/shared/screens/home-screen.tsx
+++ src/shared/screens/home-screen.tsx
@@ -1,21 +1,104 @@
-import { Button, Flex, Text } from "@mantine/core";
-import { TodoList } from "@/features/todo/ui";
-import { useGlobalModals } from "@/shared/modals/app";
-import { IModalType } from "../modals/domain";
+import {
+	Box,
+	Button,
+	Card,
+	Flex,
+	Grid,
+	GridCol,
+	type MantineColor,
+	Paper,
+	Text,
+	ThemeIcon,
+	Title,
+} from "@mantine/core";
+import { type ReactNode, useMemo } from "react";
+import { BankNotesIcon, FireIcon, MoonIcon, SunIcon } from "@/shared/icons";
+
+const STATS: {
+	name: string;
+	icon: ReactNode;
+	amount: number;
+	insight: string;
+	color: MantineColor;
+}[] = [
+	{
+		name: "Total Expenses",
+		icon: <FireIcon />,
+		amount: 100,
+		insight: "Increased",
+		color: "green",
+	},
+	{
+		name: "Today Expenses",
+		icon: <SunIcon />,
+		amount: -20,
+		insight: "Decreased",
+		color: "blue",
+	},
+	{
+		name: "New Expenses",
+		icon: <MoonIcon />,
+		amount: 56,
+		insight: "Increased",
+		color: "red",
+	},
+	{
+		name: "Purchases",
+		icon: <BankNotesIcon />,
+		amount: -27,
+		insight: "Decreased",
+		color: "yellow",
+	},
+];
 
 export default function HomeScreen() {
-	const modals = useGlobalModals();
+	const today = useMemo(() => {
+		const date = new Date();
+		return date.toDateString();
+	}, []);
 
 	return (
-		<Flex direction="column" gap="md">
-			<Text size="xl" fw="bold" display="block">
-				📌 Todo List
-			</Text>
-			<TodoList />
-
-			<Button onClick={() => modals.set({ type: IModalType.TEST })}>
-				Open Test Modal
-			</Button>
+		<Flex direction="column" gap="lg">
+			<Box>
+				<Title size="h2">Dashboard</Title>
+				<Text c="dimmed" size="sm">
+					{today}
+				</Text>
+			</Box>
+			<Paper p="md" withBorder>
+				<Flex
+					direction="row"
+					gap="md"
+					justify="space-between"
+					mb="sm"
+					wrap="wrap"
+				>
+					<Title size="h3">All Expenses</Title>
+					<Button>Add Expense</Button>
+				</Flex>
+				<Grid gutter="md">
+					{STATS.map((stat) => (
+						<GridCol key={stat.name} span={{ base: 12, xs: 6, md: 3 }}>
+							<Card withBorder>
+								<Flex direction="column" gap="md">
+									<Flex align="center" direction="row" gap="sm">
+										<ThemeIcon color={stat.color}>{stat.icon}</ThemeIcon>
+										<Title size="h5">{stat.name}</Title>
+									</Flex>
+									<Text fw="bold" size="1.5rem">
+										$ {stat.amount}
+									</Text>
+									<Text c="dimmed" size="xs" ta="right">
+										<ThemeIcon color={stat.color} bdrs="100%" size="0.6rem" />{" "}
+										{stat.insight}
+									</Text>
+								</Flex>
+							</Card>
+						</GridCol>
+					))}
+				</Grid>
+			</Paper>
+			<Paper p="md" withBorder></Paper>
 		</Flex>
 	);
 }
