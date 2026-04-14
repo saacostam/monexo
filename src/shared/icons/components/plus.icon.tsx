41f5ca760cf8010af2e7ae7b1d39ca70982d1964
saacostam <73088662+saacostam@users.noreply.github.com>
2026-04-14 15:43:58 -0500

    feat(icons): add plus icon

---
+++ src/shared/icons/components/plus.icon.tsx
@@ -0,0 +1,21 @@
+import type { BaseIconProps } from "../types";
+
+export function PlusIcon(props: BaseIconProps) {
+	return (
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			fill="none"
+			viewBox="0 0 24 24"
+			strokeWidth={1.5}
+			stroke="currentColor"
+			{...props}
+		>
+			<title>Plus Icon</title>
+			<path
+				strokeLinecap="round"
+				strokeLinejoin="round"
+				d="M12 4.5v15m7.5-7.5h-15"
+			/>
+		</svg>
+	);
+}
