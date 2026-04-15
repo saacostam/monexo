deaad86bf35c32aac6722a760a82273a77892c95
saacostam <73088662+saacostam@users.noreply.github.com>
2026-04-15 01:29:40 -0500

    feat: add mantine charts

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+
 import "@mantine/core/styles.css";
+import "@mantine/charts/styles.css";
 import "@mantine/notifications/styles.css";
+
 import "./index.css";
 import App from "./App.tsx";
 
