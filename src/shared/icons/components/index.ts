4a0067b509fb2fed82ce77c5ad116a39d8134d45
saacostam <73088662+saacostam@users.noreply.github.com>
2026-04-17 22:43:21 -0500

    feat(icons): add calendar date range icon

--- src/shared/icons/components/index.ts
+++ src/shared/icons/components/index.ts
@@ -2,6 +2,7 @@ export * from "./arrow-left.icon";
 export * from "./arrow-right.icon";
 export * from "./arrow-right-start-on-rectangle.icon";
 export * from "./bank-notes.icon";
+export * from "./calendar-date-range.icon";
 export * from "./chart-bar.icon";
 export * from "./chevron-down.icon";
 export * from "./chevron-up.icon";
