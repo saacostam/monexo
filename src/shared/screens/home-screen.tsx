8847c3bbc417b865a140beb6b0092b9faa45d5c6
saacostam <73088662+saacostam@users.noreply.github.com>
2026-04-17 22:45:11 -0500

    feat(home): add calendar range input

--- src/shared/screens/home-screen.tsx
+++ src/shared/screens/home-screen.tsx
@@ -1,68 +1,39 @@
-import { Button, Card, Flex, Grid, GridCol, Text } from "@mantine/core";
+import { Box, Button, Flex, Grid, GridCol, Text, Title } from "@mantine/core";
+import { DatePickerInput } from "@mantine/dates";
 import { ExpensesBreakdown } from "@/features/expense/breakdown/ui";
 import { ExpensesStats } from "@/features/expense/stats/ui";
 import { ExpensesTable } from "@/features/expense/table/ui";
-import { ArrowLeftIcon, ArrowRightIcon, PlusIcon } from "@/shared/icons";
-
-const TIME_RANGES: {
-	label: string;
-}[] = [
-	{
-		label: "Month",
-	},
-	{
-		label: "Week",
-	},
-	{
-		label: "Day",
-	},
-];
+import { CalendarDateRangeIcon, PlusIcon } from "@/shared/icons";
 
 export default function HomeScreen() {
 	return (
 		<Flex direction="column" gap="lg">
-			<Card withBorder>
-				<Flex
-					align="center"
-					direction="row"
-					justify="space-between"
-					wrap="wrap"
-					gap="md"
+			<Flex
+				align="end"
+				direction="row"
+				justify="space-between"
+				wrap="wrap"
+				gap="md"
+			>
+				<Box>
+					<Title size="h2">Dashboard</Title>
+					<Text c="dimmed" size="sm">
+						Select a date range to explore.
+					</Text>
+				</Box>
+				<Button
+					leftSection={<PlusIcon height="1.2rem" width="1.2rem" />}
+					size="md"
 				>
-					<Flex align="center" direction="column" gap="md">
-						<Flex align="center" direction="row" gap="xs" wrap="wrap">
-							<Button size="compact-sm">
-								<ArrowLeftIcon height="1rem" width="1rem" />
-							</Button>
-							<Text fw="bold" size="1.6rem">
-								April / 2026
-							</Text>
-							<Button size="compact-sm">
-								<ArrowRightIcon height="1rem" width="1rem" />
-							</Button>
-						</Flex>
-						<Flex direction="row" gap="xs" wrap="wrap">
-							{TIME_RANGES.map(({ label }) => (
-								<Button
-									key={label}
-									size="compact-sm"
-									variant={
-										label === TIME_RANGES.at(0)?.label ? "filled" : "light"
-									}
-								>
-									{label}
-								</Button>
-							))}
-						</Flex>
-					</Flex>
-					<Button
-						leftSection={<PlusIcon height="1.2rem" width="1.2rem" />}
-						size="md"
-					>
-						Add Expense
-					</Button>
-				</Flex>
-			</Card>
+					Add Expense
+				</Button>
+			</Flex>
+			<DatePickerInput
+				leftSection={<CalendarDateRangeIcon height="1.5rem" width="1.5rem" />}
+				placeholder="Date"
+				size="md"
+				type="range"
+			/>
 			<ExpensesStats />
 			<Grid>
 				<GridCol span={{ base: 12, md: 8 }}>
