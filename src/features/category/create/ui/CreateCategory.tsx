d526b249436b409e2d5358c2e33a2812e81585f6
saacostam <73088662+saacostam@users.noreply.github.com>
2026-04-19 00:40:51 -0500

    feat(category): extensible create category comp

---
+++ src/features/category/create/ui/CreateCategory.tsx
@@ -0,0 +1,51 @@
+import { useCallback } from "react";
+import { useMutateCreateCategory } from "@/features/category/core/app";
+import type { ICategoryClientPayload } from "@/features/category/core/domain";
+import {
+	type IManageCategoryForm,
+	useManageCategory,
+} from "@/features/category/manage-category/app";
+import { ManageCategory } from "@/features/category/manage-category/ui";
+
+export interface CreateCategoryProps {
+	onError: (e: unknown) => void;
+	onSuccess: (res: ICategoryClientPayload["CreateResponse"]) => void;
+	onSettled: () => void;
+}
+
+export function CreateCategory({
+	onError,
+	onSuccess,
+	onSettled,
+}: CreateCategoryProps) {
+	const form = useManageCategory({
+		defaultValues: { name: "", description: "" },
+	});
+
+	const createCategory = useMutateCreateCategory();
+
+	const onSubmit = useCallback(
+		(data: IManageCategoryForm) => {
+			createCategory.mutate(
+				{
+					...data,
+				},
+				{
+					onSuccess,
+					onError,
+					onSettled,
+				},
+			);
+		},
+		[createCategory.mutate, onError, onSuccess, onSettled],
+	);
+
+	return (
+		<ManageCategory
+			action="Create"
+			form={form}
+			isPending={createCategory.isPending}
+			onSubmit={onSubmit}
+		/>
+	);
+}
