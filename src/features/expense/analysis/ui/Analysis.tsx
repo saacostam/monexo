49789f5b7e84f17d2a9fce7ffeac5fb4d06a255b
saacostam <73088662+saacostam@users.noreply.github.com>
2026-05-13 22:26:13 -0500

    feat(analysis): add ExpensesBreakdown to analysis

--- src/features/expense/analysis/ui/Analysis.tsx
+++ src/features/expense/analysis/ui/Analysis.tsx
@@ -1,6 +1,8 @@
-import { Skeleton } from "@mantine/core";
+import { Card, Divider, Flex, Grid, ThemeIcon, Title } from "@mantine/core";
 import type { DatesRangeValue } from "@mantine/dates";
 import { DateRangeInput } from "@/features/date/ui";
+import { ExpensesBreakdown } from "@/features/expense/breakdown/ui";
+import { ChartBarIcon } from "@/shared/icons";
 
 export interface AnalysisProps {
 	dateRange: DatesRangeValue<string>;
@@ -11,7 +13,20 @@ export function Analysis({ dateRange, setDateRange }: AnalysisProps) {
 	return (
 		<>
 			<DateRangeInput dateRange={dateRange} setDateRange={setDateRange} />
-			<Skeleton h="256px" />
+			<Grid>
+				<Grid.Col span={{ base: 12, sm: 6 }}>
+					<Card h="100%" withBorder>
+						<Flex align="center" direction="row" gap="xs" wrap="wrap">
+							<ThemeIcon bdrs="100%" p="0.25rem">
+								<ChartBarIcon />
+							</ThemeIcon>
+							<Title size="h4">Spending Breakdown</Title>
+						</Flex>
+						<Divider my="sm" />
+						<ExpensesBreakdown dateRange={dateRange} />
+					</Card>
+				</Grid.Col>
+			</Grid>
 		</>
 	);
 }
