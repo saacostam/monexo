24c9460c1a6a22f1cbfd971dd6e30efae1e60243
saacostam <73088662+saacostam@users.noreply.github.com>
2026-05-18 23:51:23 -0500

    fix(AppLayout): remove un-used navbar

--- src/shared/layout/ui/app-layout.tsx
+++ src/shared/layout/ui/app-layout.tsx
@@ -1,13 +1,11 @@
 import {
 	AppShell,
-	Burger,
 	Container,
 	Flex,
 	Group,
 	UnstyledButton,
 } from "@mantine/core";
-import { useDisclosure } from "@mantine/hooks";
-import { type PropsWithChildren, useMemo } from "react";
+import type { PropsWithChildren } from "react";
 import { Link } from "react-router";
 import { ThemeToggle } from "@/features/theme/ui";
 import { UserMenu } from "@/features/user/menu/ui";
@@ -16,40 +14,13 @@ import { IThemeVariant } from "@/shared/adapters/theme/domain";
 import { Logo } from "@/shared/components";
 import { genRoute, RouteName } from "@/shared/router/app";
 
-const MAIN_LINKS: {
-	name: RouteName;
-	label: string;
-}[] = [] as const;
-
 export function AppLayout({ children }: PropsWithChildren) {
 	const { themeAdapter } = useAdapters();
 
-	const [opened, { toggle }] = useDisclosure();
-
-	const links = useMemo(
-		() =>
-			MAIN_LINKS.map(({ name, label }) => ({
-				href: genRoute({
-					name,
-				}),
-				label,
-			})),
-		[],
-	);
-
 	return (
-		<AppShell
-			header={{ height: 60 }}
-			navbar={{
-				width: 300,
-				breakpoint: "sm",
-				collapsed: { desktop: true, mobile: !opened },
-			}}
-			padding="md"
-		>
+		<AppShell header={{ height: 60 }} padding="md">
 			<AppShell.Header>
 				<Group h="100%" px="md">
-					<Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" />
 					<Group justify="space-between" style={{ flex: 1 }}>
 						<UnstyledButton
 							component={Link}
@@ -59,28 +30,14 @@ export function AppLayout({ children }: PropsWithChildren) {
 						>
 							<Logo />
 						</UnstyledButton>
-						<Flex gap="lg">
-							<Group gap="lg" visibleFrom="sm">
-								{links.map(({ href, label }) => (
-									<UnstyledButton component={Link} key={href} to={href}>
-										{label}
-									</UnstyledButton>
-								))}
-							</Group>
+
+						<Flex gap="lg" align="center">
 							<ThemeToggle />
 							<UserMenu />
 						</Flex>
 					</Group>
 				</Group>
 			</AppShell.Header>
-
-			<AppShell.Navbar py="md" px="lg">
-				{links.map(({ href, label }) => (
-					<UnstyledButton component={Link} key={href} to={href}>
-						{label}
-					</UnstyledButton>
-				))}
-			</AppShell.Navbar>
 
 			<AppShell.Main
 				bg={themeAdapter.theme === IThemeVariant.LIGHT ? "gray.1" : undefined}
